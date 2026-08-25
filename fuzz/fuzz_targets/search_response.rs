bbcccdee350dd9b3b593d6df1d55df9d0e97b6f2
Yuxin Qiao <104957188+Yuxin-Qiao@users.noreply.github.com>
2026-08-26 01:49:00 +0800

    test: fuzz search response decoding

---
+++ fuzz/fuzz_targets/search_response.rs
@@ -0,0 +1,8 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let text = String::from_utf8_lossy(data);
+    let _ = kumone_core::search::decode_song_search_response(&text);
+});
